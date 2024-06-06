7630037f3704e78f609f75837d8f1dcc5d9d63c5
jakariamasum <jmphy698@gmail.com>
2024-06-06 23:02:18 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <p className="text-sky-400/100">The quick brown fox...</p>
+      <p className="text-sky-400/75">The quick brown fox...</p>
+      <p className="text-sky-400/50">The quick brown fox...</p>
+      <p className="text-sky-400/25">The quick brown fox...</p>
+      <p className="text-sky-400/0">The quick brown fox...</p>
+    </div>
+  );
+}
+
+export default App;
